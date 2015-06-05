7c656339314cd38f85ef986c6868dcc5726fe776
ErikBoesen <erik.boesen@gmail.com>
2015-06-04 21:01:42 -0400

    +GitHub social icon, -Banner, -Button

--- js/global.js
+++ js/global.js
@@ -1,11 +1,13 @@
 $(window).scroll(function() {
-    var wScroll = $(this).scrollTop();
+    // Parallax effects
+    var wScroll = $(window).scrollTop();
     $(".bg").css({
         "transform" : "translate(0,-" + wScroll / 2 + "px)"
     });
     $("header").css({
         "transform" : "translate(0," + wScroll * .65 + "px)"
     });
+    menuClose(); //Close menu
 });
 
 var isMenuOpen = false;
@@ -34,31 +36,19 @@ var menuClose = function() {
 };
 var menu = function() {
     $(".menuButton").click(function() {
-        if (isMenuOpen === false) {menuOpen()}
-        else {menuClose()}
+        if (isMenuOpen === false) {menuOpen();}
+        else {menuClose();}
     });
 };
 
-// Close menu on scroll
-window.onscroll = function() {
-    $("nav").animate({
-        left: "-240px"
-    }, 200);
-    $(".menuButton").animate({
-        left: "0px"
-    }, 200);
-
-    isMenuOpen = false;
-};
-
-$(document).ready(menu);
+addEventListener("load", menu);
 
 // Email address mask
 var emailMe = function() {
     window.open("mailto:erikboesen@erikboesen.com");
 };
 
-/* Google Analytics */
+// Google Analytics
 if(document.location.protocol=='http:'){
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
