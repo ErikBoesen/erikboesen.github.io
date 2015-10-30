b5937fafb71cf4da75c419c39cefe3ec023dc482
Erik Boesen <erik.boesen@gmail.com>
2015-10-30 15:44:37 -0400

    Made code comply with Viget FED standards (github.com/greypants/FED-docs)

--- js/global.js
+++ js/global.js
@@ -1,58 +1,68 @@
 $(window).scroll(function() {
     // Parallax effects
     var wScroll = $(window).scrollTop();
-    $(".bg").css({
-        "transform" : "translate(0,-" + wScroll / 2 + "px)"
+    $('.bg').css({
+        'transform': 'translate(0,-' + wScroll / 2 + 'px)'
     });
-    $("header").css({
-        "transform" : "translate(0," + wScroll * .65 + "px)"
+    $('header').css({
+        'transform': 'translate(0,' + wScroll * 0.65 + 'px)'
     });
 });
 
 var isMenuOpen = false;
 
 var menuOpen = function() {
-    $("nav").stop();
-    $("nav").animate({
-        left: "0px"
+    $('nav').stop();
+    $('nav').animate({
+        left: '0px'
     }, 300);
-    $(".menuButton").animate({
-        left: "-20px"
+    $('.menuButton').animate({
+        left: '-20px'
     }, 200);
 
     isMenuOpen = true;
 };
 var menuClose = function() {
-    $("nav").stop();
-    $("nav").animate({
-        left: "-1000px"
+    $('nav').stop();
+    $('nav').animate({
+        left: '-1000px'
     }, 300);
-    $(".menuButton").animate({
-        left: "10px"
+    $('.menuButton').animate({
+        left: '10px'
     }, 200);
 
     isMenuOpen = false;
 };
 var menu = function() {
-    $(".menuButton").click(function() {
-        if (isMenuOpen === false) {menuOpen();}
-        else {menuClose();}
+    $('.menuButton').click(function() {
+        if (isMenuOpen === false) {
+            menuOpen();
+        } else {
+            menuClose();
+        }
     });
 };
 
-addEventListener("load", menu);
+addEventListener('load', menu);
 
 // Email address mask
 var emailMe = function() {
-    window.open("mailto:erikboesen@erikboesen.com");
+    window.open('mailto:erikboesen@erikboesen.com');
 };
 
 // Google Analytics
-if(document.location.protocol=='http:'){
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+if (document.location.protocol == 'http:') {
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
     ga('create', 'UA-44442416-1', 'auto');
     ga('send', 'pageview');
