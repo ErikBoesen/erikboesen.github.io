d6cfce296553ef020a4fea41ede142fddf5eccc8
ErikBoesen <me@erikboesen.com>
2016-04-18 17:28:48 -0400

    Solid colors in bg of homepage, gradients on 404

--- scripts.js
+++ scripts.js
@@ -11,19 +11,3 @@ if(document.location.protocol=='http:'){
       ga('create', 'UA-44442416-1', 'auto');
       ga('send', 'pageview');
 }
-
-var angle = 0;
-var c = [
-    [0, 136, 122],
-    [86, 199, 201]
-];
-setInterval(function() {
-    document.body.style.background = 'linear-gradient(' + angle + 'deg, rgb(' + c[0][0] + ',' + c[0][1] + ',' + c[0][2] + '), rgb(' + c[1][0] + ',' + c[1][1] + ',' + c[1][2] + ')';
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 3; j++) {
-            c[i][j] = c[i][j] + (Math.ceil(Math.random() * 41) - 21);
-            console.log(Math.ceil(Math.random() * 41) - 21);
-        }
-    }
-    angle++;
-}, 1);
