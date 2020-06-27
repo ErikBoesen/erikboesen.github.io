ed7db5c4b7a25926582862e60859cbe51ddd3929
ErikBoesen <me@erikboesen.com>
2020-06-27 17:18:45 -0400

    Stop using ø

--- js/home.js
+++ js/home.js
@@ -1,6 +1,3 @@
 var ADDR = 'me',
     DOMAIN = 'erikboesen.com';
 document.getElementById('email').href = 'mailto' + ':' + ADDR + '@' + DOMAIN;
-
-document.getElementById('name').onmouseenter = function() { this.textContent = 'ERIK BOESEN'; }
-document.getElementById('name').onmouseout   = function() { this.textContent = 'ERIK BØSEN';  }
