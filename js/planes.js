c54562184db253a238babe8826e8580532aec35e
ErikBoesen <me@erikboesen.com>
2022-04-02 17:02:05 -0400

    Add planes.js

---
+++ js/planes.js
@@ -0,0 +1,119 @@
+const canvas = document.getElementsByTagName('canvas')[0];
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let instructions = document.getElementById('instructions');
+
+ctx.strokeStyle = 'white';
+ctx.lineWidth = 3;
+let currentPlane = null;
+let startPosition = null;
+let planes = [];
+
+let mouseDown = false;
+let xOld, yOld, x, y;
+let mousePosition = null;
+
+const PLANE_WIDTH = 512,
+      PLANE_HEIGHT = 342,
+      PLANE_SIZE_MULTIPLIER = 0.1,
+      PLANE_DRAW_WIDTH = parseInt(PLANE_WIDTH * PLANE_SIZE_MULTIPLIER),
+      PLANE_DRAW_HEIGHT = parseInt(PLANE_HEIGHT * PLANE_SIZE_MULTIPLIER);
+let planeImageLeft = new Image(PLANE_WIDTH, PLANE_HEIGHT);
+planeImageLeft.src = 'plane_left.png';
+let planeImageRight = new Image(PLANE_WIDTH, PLANE_HEIGHT);
+planeImageRight.src = 'plane_right.png';
+
+const PLANE_SPEED = 0.06;
+const G = 0.05;
+
+canvas.onmousedown = function(e) {
+    console.log('Mouse down!');
+    if (instructions) {
+        instructions.parentNode.removeChild(instructions);
+        instructions = null;
+    }
+    mouseDown = true;
+    currentPlane = {
+        x: mousePosition.x,
+        y: mousePosition.y,
+    };
+
+}
+canvas.onmouseup = function(e) {
+    console.log('Mouse up!');
+    mouseDown = false;
+    currentPlane.xSpeed = PLANE_SPEED * (currentPlane.x - mousePosition.x);
+    currentPlane.ySpeed = PLANE_SPEED * (currentPlane.y - mousePosition.y);
+    planes.push(currentPlane);
+    currentPlane = null;
+}
+canvas.onmousemove = function(e) {
+    mousePosition = {
+        x: e.clientX - canvas.offsetLeft,
+        y: e.clientY - canvas.offsetTop
+    };
+    if (mouseDown) {
+    }
+}
+
+function move() {
+    planes = planes.filter(function(plane) {
+        return (
+            plane.x > 0 - PLANE_DRAW_WIDTH &&
+            plane.x < canvas.width + PLANE_DRAW_WIDTH &&
+            plane.y < canvas.height + PLANE_DRAW_HEIGHT
+        );
+    });
+    for (plane of planes) {
+        plane.x += plane.xSpeed;
+        plane.y += plane.ySpeed;
+        plane.ySpeed += G;
+    }
+}
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = '#000000';
+    ctx.lineWidth = 1;
+    shownPlanes = planes.slice();
+    if (mouseDown && currentPlane != null) {
+        let lineDashLength = Math.sqrt(Math.pow(currentPlane.x - mousePosition.x, 2) +
+                              Math.pow(currentPlane.y - mousePosition.y, 2)) * 0.05;
+        ctx.setLineDash([lineDashLength, lineDashLength / 2]);
+        ctx.beginPath();
+        ctx.moveTo(currentPlane.x, currentPlane.y);
+        ctx.lineTo(mousePosition.x, mousePosition.y);
+        ctx.stroke();
+    }
+    if (currentPlane) {
+        shownPlanes.push(currentPlane);
+    }
+    for (plane of shownPlanes) {
+        ctx.save();
+        ctx.translate(plane.x, plane.y);
+        let xOffset, yOffset;
+        if (plane == currentPlane) {
+            xOffset = plane.x - mousePosition.x;
+            yOffset = plane.y - mousePosition.y;
+        } else {
+            xOffset = plane.xSpeed;
+            yOffset = plane.ySpeed;
+        }
+        let planeLeft = (xOffset < 0);
+        let angle = planeLeft ?
+                        -Math.atan2(yOffset, -xOffset) :
+                        Math.atan2(yOffset, xOffset);
+        angle += (planeLeft ? -Math.PI : Math.PI) / 6;
+        ctx.rotate(angle);
+        ctx.drawImage(planeLeft ? planeImageLeft : planeImageRight,
+                      -PLANE_DRAW_WIDTH / 2, -PLANE_DRAW_HEIGHT / 2,
+                      PLANE_DRAW_WIDTH, PLANE_DRAW_HEIGHT);
+        ctx.restore();
+    }
+}
+setInterval(function() {
+    move();
+    draw();
+}, 20);
